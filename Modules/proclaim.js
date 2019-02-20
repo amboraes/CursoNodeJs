e79f43c970c97a50c16375e5d4d1aeb3f07cd4eb
Tomas <amboraes@hotmail.com>
2019-02-19 22:00:59 -0500

    inicios curso

---
+++ Modules/proclaim.js
@@ -0,0 +1,8 @@
+var whisper = function(message){
+  console.log('proclaiming: ' + message);
+};
+exports.softly= whisper;
+
+exports.loudly = function (message){
+  console.log('PROCLAIMING: ' + message);
+};
